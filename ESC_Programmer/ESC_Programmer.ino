7759748765ce907eeca1487cbb3682ff88bef202
kea64 <kea64@drexel.edu>
2015-01-19 22:28:40 -0500

    ESC Tuner
    Basic ESC tuner to adjust all ESC's at once without unplugging wires.
    Needs a remote and some care.

---
+++ ESC_Programmer/ESC_Programmer.ino
@@ -0,0 +1,105 @@
+//ESC Adjuster
+
+#include <Servo.h>
+#include <PinChangeInt.h>
+
+#define aileronPin A3
+#define elevatorPin A2
+#define throttlePin A1
+#define rudderPin A0
+
+#define channel1 2
+#define channel2 3
+#define channel3 4
+#define channel4 5
+#define channel5 6
+#define channel6 7
+
+volatile int channel1Cycle;
+volatile int channel2Cycle;
+volatile int channel3Cycle;
+volatile int channel4Cycle;
+volatile int channel5Cycle;
+volatile int channel6Cycle;
+
+unsigned long channel1Start,channel2Start,channel3Start,channel4Start,channel5Start,channel6Start;
+
+Servo output1;
+Servo output2;
+Servo output3;
+Servo output4;
+
+void setup(){
+   Serial.begin(38400); 
+   
+   pinMode(channel1,INPUT);digitalWrite(channel1,HIGH);PCintPort::attachInterrupt(channel1,&channel1Interrupt,CHANGE);
+   pinMode(channel2,INPUT);digitalWrite(channel2,HIGH);PCintPort::attachInterrupt(channel2,&channel2Interrupt,CHANGE);
+   pinMode(channel3,INPUT);digitalWrite(channel3,HIGH);PCintPort::attachInterrupt(channel3,&channel3Interrupt,CHANGE);
+   pinMode(channel4,INPUT);digitalWrite(channel4,HIGH);PCintPort::attachInterrupt(channel4,&channel4Interrupt,CHANGE);
+   pinMode(channel5,INPUT);digitalWrite(channel5,HIGH);PCintPort::attachInterrupt(channel5,&channel5Interrupt,CHANGE);
+   pinMode(channel6,INPUT);digitalWrite(channel6,HIGH);PCintPort::attachInterrupt(channel6,&channel6Interrupt,CHANGE);
+ 
+   output1.attach(aileronPin);
+   output2.attach(elevatorPin);
+   output3.attach(throttlePin);
+   output4.attach(rudderPin);
+}
+
+
+
+void loop(){
+  
+  output1.writeMicroseconds(channel3Cycle);
+  output2.writeMicroseconds(channel3Cycle);
+  output3.writeMicroseconds(channel3Cycle);
+  output4.writeMicroseconds(channel4Cycle);
+  
+}
+
+void channel1Interrupt(){
+  if (digitalRead(channel1) == 1){
+      channel1Start = micros();
+  } else {
+      channel1Cycle = micros() - channel1Start;
+  }
+}
+
+void channel2Interrupt(){
+  if (digitalRead(channel2) == 1){
+      channel2Start = micros();
+  } else {
+      channel2Cycle = micros() - channel2Start;
+  }
+}
+
+void channel3Interrupt(){
+  if (digitalRead(channel3) == 1){
+      channel3Start = micros();
+  } else {
+      channel3Cycle = micros() - channel3Start;
+  }
+}
+
+void channel4Interrupt(){
+  if (digitalRead(channel4) == 1){
+      channel4Start = micros();
+  } else {
+      channel4Cycle = micros() - channel4Start;
+  }
+}
+
+void channel5Interrupt(){
+  if (digitalRead(channel5) == 1){
+      channel5Start = micros();
+  } else {
+      channel5Cycle = micros() - channel5Start;
+  }
+}
+
+void channel6Interrupt(){
+  if (digitalRead(channel6) == 1){
+      channel6Start = micros();
+  } else {
+      channel6Cycle = micros() - channel6Start;
+  }
+}
