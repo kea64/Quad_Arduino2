3a531860e87dbb4524e9b49c6977ae699de6bd20
kea64 <kea64@drexel.edu>
2015-11-07 09:02:23 -0500

    HC-SR04 Samples
    Sample ultrasonic sensor files. To be expanded into a full-fledged
    library soon.

---
+++ HC-SR04_V2/HC-SR04_V2.ino
@@ -0,0 +1,48 @@
+#include <PinChangeInt.h>
+
+#define trigPin1 3
+#define echoPin1 2
+
+#define cutOffTime 24000
+
+volatile int ping1Dist = -1;
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(echoPin1,INPUT);digitalWrite(echoPin1,HIGH);PCintPort::attachInterrupt(echoPin1,&pingInterrupt1,CHANGE);
+  pinMode(trigPin1, OUTPUT); digitalWrite(trigPin1, LOW);
+  
+}
+
+void loop() {
+  ping1Start();
+  delay(40);
+  Serial.print("Distance: ");
+  Serial.println(ping1Dist);
+  
+  delay(50);
+  
+
+}
+
+void ping1Start(){
+  ping1Dist = 0;
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+}
+
+void pingInterrupt1(){
+  static unsigned long startClock;
+  if (digitalRead(echoPin1) == HIGH){
+    startClock = micros();
+  } else {
+    int cycle = micros() - startClock;
+    if (cycle <= cutOffTime){
+      ping1Dist = cycle / 58.2;
+    } else {
+      ping1Dist = -1;
+    }
+  }
+}
