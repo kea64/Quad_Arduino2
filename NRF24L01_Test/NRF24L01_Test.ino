6df94c9209002fd192ff50ba90cc29a340039f32
kea64 <kea64@drexel.edu>
2016-02-10 21:53:11 -0500

    Pipeline NRF Stuff

--- NRF24L01_Test/NRF24L01_Test.ino
+++ NRF24L01_Test/NRF24L01_Test.ino
@@ -4,6 +4,9 @@
 #include "RF24.h"
 #include "H2_EEPROM.h"
 
+#define TIMEOUT_RX 100
+#define SENSOR_PIN 3
+
 RF24 radio(9, 10);
 
 const uint64_t talking_pipes[5] = { 0xF0F0F0F0D2LL, 0xF0F0F0F0C3LL, 0xF0F0F0F0B4LL, 0xF0F0F0F0A5LL, 0xF0F0F0F096LL };
@@ -15,6 +18,10 @@ void setup() {
   Serial.begin(115200);
   Serial.println("NRF24L01 Test");
 
+  //Sensor Setup
+  pinMode(SENSOR_PIN, INPUT);
+  digitalWrite(SENSOR_PIN, LOW);
+  
   radio.begin();
 
   if (EEPROM.read(FC_ADDRESS) == 1){
@@ -45,13 +52,54 @@ void loop() {
 
 void TX_PROCESS(){
   if (EEPROM.read(FC_ADDRESS) >= 2 && EEPROM.read(FC_ADDRESS) <= 6){
+    radio.stopListening();
 
+    uint8_t stuff;
+    if (digitalRead(SENSOR_PIN)){
+      stuff = 1;
+    } else {
+      stuff = 0;
+    }
+    radio.write(&stuff, sizeof(stuff));
+    radio.startListening();
+    bool timeout = false;
+    unsigned long start_rx = millis();
+    while (!radio.available() && !timeout){
+      if (millis() - start_rx > TIMEOUT_RX){
+        timeout = true;
+      }
+    }
+    if (timeout){
+      Serial.println("No Response");
+    } else {
+      uint8_t data;
+      radio.read(&data, sizeof(data));
+      Serial.println(data);
+    }
+
+    delay(1000);
   }
 }
 
 void RX_PROCESS(){
   if (EEPROM.read(FC_ADDRESS) == 1){
-    
+    uint8_t pipe_num;
+    if (radio.available(&pipe_num)){
+      uint8_t data;
+      bool done = false;
+      while(!done){
+        done = radio.read(&data, sizeof(data));
+        Serial.println(data);
+      }
+
+      radio.stopListening();
+      radio.openWritingPipe(listening_pipes[pipe_num - 1]);
+
+      uint8_t stuff = EEPROM.read(FC_ADDRESS);
+      radio.write( &stuff, sizeof(stuff) );
+
+      radio.startListening();
+    }
   }
 }
 
